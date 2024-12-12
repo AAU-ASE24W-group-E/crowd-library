a5d45e4525eecd6bcb4b525ea3e8b6ed8b594230
Ramiz Ismailov <ramiz.ismailov@skiline.cc>
2024-12-12 11:23:58 +0100

    fix minikube script

--- k8s/minikube/startminikube.sh
+++ k8s/minikube/startminikube.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+mkdir -p /tmp/k8s-data
+
 minikube start --cpus='4' --memory='4g' --disk-size='8g' \
+  --driver='docker' \
   --mount=true --mount-string='/tmp/k8s-data:/tmp/k8s-data' \
-  --ports='80,8080,8443,5432' \
-  --static-ip='192.168.200.200'
+  --ports='80,8080,8443,5432'
 
 echo "Enabling addons..."
 minikube addons enable ingress
