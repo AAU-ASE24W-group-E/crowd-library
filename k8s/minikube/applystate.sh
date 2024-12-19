fff62516a9920c50f1ebe9369ae3bcf5be617762
Ramiz Ismailov <ramiz.ismailov@skiline.cc>
2024-12-19 20:45:04 +0100

    workaround missing pushd/popd on ci runner

--- k8s/minikube/applystate.sh
+++ k8s/minikube/applystate.sh
@@ -2,7 +2,8 @@
 
 echo "Note: kubectl must be available, e.g. alias kubectl='minikube kubectl -- ' "
 
-pushd ../state
+#pushd ../state
+cd ../state
 
 for f in *.yaml; do
   echo "Applying $f..."
@@ -10,7 +11,9 @@ for f in *.yaml; do
 done
 
 echo "Done."
-popd
+
+#popd
+cd ../minikube
 
 echo "Waiting for pods to be ready..."
 kubectl -n cl wait --for=condition=Ready --timeout=30s pod -l name=book-service
