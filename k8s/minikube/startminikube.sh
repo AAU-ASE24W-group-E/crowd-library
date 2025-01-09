db8c0ecff7801817497b64c79003fb2293c7664b
Selina Isak <seisak@edu.aau.at>
2025-01-09 21:17:02 +0100

    Adjust for Windows Users

--- k8s/minikube/startminikube.sh
+++ k8s/minikube/startminikube.sh
@@ -2,9 +2,9 @@
 
 mkdir -p /tmp/k8s-data
 
-minikube start --cpus='4' --memory='4g' --disk-size='8g' \
+minikube start --cpus='4' --memory='3666MB' --disk-size='8g' \
   --driver='docker' \
-  --mount=true --mount-string='/tmp/k8s-data:/tmp/k8s-data' \
+  --mount=true --mount-string='//tmp/k8s-data:/tmp/k8s-data' \
   --ports='8080,8443,5432'
 
 echo "Enabling addons..."
