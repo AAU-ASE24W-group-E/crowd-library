3afa96de8cf413dffa21f39778a15be76f09026c
Ramiz Ismailov <ramiz.ismailov@skiline.cc>
2025-01-10 08:19:26 +0100

    add postgres with postgis. fix minikube startup issues. add local .env file support in startminikube.sh

--- k8s/minikube/startminikube.sh
+++ k8s/minikube/startminikube.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
 
-mkdir -p /tmp/k8s-data
+if [ -f .env ]; then
+  source .env
+else
+  touch .env
+  echo "Created empty .env file which you can use to override default settings."
+fi
 
-minikube start --cpus='4' --memory='3666MB' --disk-size='8g' \
+# you can override these variables in your .env file
+DOCKER_CPUS=${DOCKER_CPUS:-4}
+DOCKER_MAX_MEMORY=${DOCKER_MAX_MEMORY:-4g}
+DOCKER_DISK_SIZE=${DOCKER_DISK_SIZE:-8g}
+DOCKER_PORTS=${DOCKER_PORTS:-80,8080,8443,5432}
+
+DATADIR="$(pwd)/tmp/k8s-data"
+echo "Mounting data directory: $DATADIR"
+mkdir -p $DATADIR
+
+minikube start --cpus="$DOCKER_CPUS" --memory="$DOCKER_MAX_MEMORY" --disk-size="$DOCKER_DISK_SIZE" \
   --driver='docker' \
-  --mount=true --mount-string='//tmp/k8s-data:/tmp/k8s-data' \
-  --ports='8080,8443,5432'
+  --mount=true --mount-string="$DATADIR:/tmp/k8s-data" \
+  --ports="$DOCKER_PORTS"
 
 echo "Enabling addons..."
 minikube addons enable ingress
+minikube addons enable metrics-server
 #minikube addons enable ingress-dns
 
 echo "Aliasing kubectl to minikube..."
